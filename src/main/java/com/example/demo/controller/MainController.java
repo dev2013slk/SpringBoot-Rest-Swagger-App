134cb76b068a8045ca4c3ffac3c1fb6be920e8f8
lasathj <lasath@hatchyard.io>
2023-09-29 20:46:06 +0300

    controller added

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "Swaager 2 welcome")
 @RestController
 @RequestMapping("/")
 public class MainController {
 
+    @ApiOperation(value = "Get welcome message", response = String.class, tags = "Get Welcome Note")
     @GetMapping
     public String test(){
         return "Hello Lasath, Welcome!";
